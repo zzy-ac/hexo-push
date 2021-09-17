41019a94a0bb0058442c689408db4e3226b6005e
zzy-ac <1304024859@qq.com>
2021-09-17 16:19:47 +0800

    creat npm models

--- themes/nmz/scripts/index.js
+++ themes/nmz/scripts/index.js
@@ -17,35 +17,3 @@ function uuid() {
   }
   return S4() + S4() + '-' + S4() + '-' + S4() + '-' + S4() + '-' + S4() + S4() + S4();
 }
-
-function new_post(post) {
-  let lines = post.content.split('\n');
-  let index = lines.findIndex(item => item === 'uuid:');
-  if (index > -1) {
-    lines[index] += ' ' + uuid();
-  } else {
-    lines.splice(1, 0, 'uuid: ' + uuid());
-  }
-  post.content = lines.join('\n');
-  if (post.path !== false) {
-    fs.writeFile(post.path, post.content, () => {});
-  }
-}
-
-function before_render(post) {
-  if (post.layout == 'post' && !post.uuid) {
-    let lines = post.raw.split('\n');
-    let index = lines.findIndex(item => item === 'uuid:');
-    post.uuid = uuid();
-    if (index > -1) {
-      lines[index] += ' ' + post.uuid;
-    } else {
-      lines.splice(1, 0, 'uuid: ' + post.uuid);
-    }
-    post.raw = lines.join('\n');
-    fs.writeFile(post.full_source, post.raw, () => {});
-  }
-}
-
-hexo.on('new', new_post);
-hexo.extend.filter.register('before_post_render', before_render);
